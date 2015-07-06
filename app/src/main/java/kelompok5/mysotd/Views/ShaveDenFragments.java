f84596b03961ed83e30482a744f8d0d4499301e9
mira <mirasaraswati.jtk12@gmail.com>
2015-07-06 22:53:32 +0700

    Add floating button

--- app/src/main/java/kelompok5/mysotd/Views/ShaveDenFragments.java
+++ app/src/main/java/kelompok5/mysotd/Views/ShaveDenFragments.java
@@ -3,10 +3,14 @@ package kelompok5.mysotd.Views;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -31,6 +35,18 @@ public class ShaveDenFragments extends Fragment {
 
         ListView shavingItemListView = (ListView) rootview.findViewById(R.id.categorylistView);
         shavingItemListView.setAdapter(categoryAdapter);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) rootview.findViewById(R.id.add_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Add button selected", Toast.LENGTH_SHORT).show();
+                /*FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, new AddShavingItemFragment())
+                        .addToBackStack(null).commit();*/
+            }
+        });
 
         return rootview;
     }
